f01e332f90dfdad33c89a2384b9c90d8e57cdfc1
moreme-m <mariamlaw2011@gmail.com>
2025-07-05 17:57:12 +0100

    sql

---
+++ table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE employees(
+name TEXT,
+age INTEGER,
+salary INTEGER
+);
+
+INSERT INTO employees VALUES
+("Dante Russo",36,1000000),
+("Vuk Markovic",34,5000000),
+("Xavier Castillo",29,90000),
+("Kai Young",24,1000000),
+("Dominic Davenport",36,7000000);
+
+SELECT * FROM employees;
